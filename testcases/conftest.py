d6663f69172d73c66c39140dd199228513580f7c
Mariana Ulaieva <mulaieva@redhat.com>
2022-04-06 16:23:55 +0200

    Small fixes

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,28 +1,13 @@
-from lib2to3.pgen2 import driver
 import pytest
-from  selenium import webdriver as wd
+from selenium import webdriver as wd
 from selenium.webdriver.chrome.service import Service as chr_s
 from selenium.webdriver.firefox.service import Service as ff_s
+from selenium.webdriver.ie.service import Service as ie_s
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
 
 
-# chrome_service = chr_s(ChromeDriverManager().install())
-
-# Chrome=wd.Chrome(service=chrome_service)
-# Firefox=wd.Firefox(service=ff_service)
-
-
-# @pytest.fixture()
-# def setup(request):
-#     browsers = [Chrome,Firefox]
-#     import pdb; pdb.set_trace()
-#     for driver in browsers:
-#         driver.get("https://staging.scrive.com/t/9221714692410699950/7348c782641060a9")
-#         driver.maximize_window()
-#         yield
-#         driver.close()    
-        
 @pytest.fixture(autouse=True)
 def setup(request, browser):
     if browser == "chrome":
@@ -31,19 +16,22 @@ def setup(request, browser):
     elif browser == "ff":
         service = ff_s(GeckoDriverManager().install())
         driver = wd.Firefox(service=service)
+    elif browser == "IE":
+        service = ie_s(IEDriverManager().install())
+        driver = wd.Ie(service=service)
     else:
-        print ("provide a browser")
+        print("provide a browser")
     driver.get("https://staging.scrive.com/t/9221714692410699950/7348c782641060a9")
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
 
+
 def pytest_addoption(parser):
     parser.addoption("--browser")
+
 
 @pytest.fixture(scope="class", autouse=True)
 def browser(request):
     return request.config.getoption("--browser")
-
-
